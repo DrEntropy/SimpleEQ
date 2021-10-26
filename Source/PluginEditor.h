4900118ba6b3134466ad506ebc4cf5062aa517de
Dr Entropy <rjljr2@gmail.com>
2021-10-26 15:50:51 -0700

    Implemented Skeleton for custom look and feel
    At 11 minute mark in video

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,14 +11,44 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-class CustomRotarySlider : public juce::Slider
+
+// how the fark did he figure out this dark hole
+struct LookAndFeel : juce::LookAndFeel_V4 {
+    void  drawRotarySlider (juce::Graphics&,
+                            int x, int y, int width, int height,
+                            float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, juce::Slider &) override {}
+    
+};
+
+
+
+class RotarySliderWithLabels : public juce::Slider
 {
 public:
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap,const juce::String& unitSuffix) :
+     juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+     juce::Slider::TextEntryBoxPosition::NoTextBox),
+     param(&rap),
+     suffix(unitSuffix)
     {
-        
+        setLookAndFeel(&lnf);
     }
+    
+    
+    ~RotarySliderWithLabels() {
+        setLookAndFeel(nullptr);
+    }
+    // custom visuals
+    void paint(juce::Graphics& g) override{}
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const {return 14;}
+    juce::String getDisplayString() const;
+    
+    
+private:
+    LookAndFeel lnf;
+    juce::RangedAudioParameter* param;
+    juce::String suffix;  // for example 'Hz'
 };
 
 
@@ -62,7 +92,7 @@ private:
     
     
     
-    CustomRotarySlider peakFreqSlider,
+    RotarySliderWithLabels peakFreqSlider,
     peakGainSlider,
     peakQualitySlider,
     lowCutFreqSlider,
